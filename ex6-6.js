6e1cdc03d0f669abb2a071d0ef9655673620fc88
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2022-10-11 10:46:22 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,4 @@
 var who66='world';
-function sayhello66(){alert(who65+'が来た');}
-function taro65(){who65='太郎';}
-function hanako65(){who65='花子';}
+function sayhello66(){alert(who66+'が来た');}
+function taro65(){who66='太郎';}
+function hanako65(){who66='花子';}

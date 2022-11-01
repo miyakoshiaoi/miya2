6d101beb430aa5f46000725f6c5b7323a8b0a01f
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2022-11-01 10:26:52 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,1 @@
-function gazouhennkou() {alert(document.getElementById('城西大学のロゴ').innerHTML);}
-<img src="./image/ロゴ1.jpg" alt="城西大学ロゴ" width="420" height="220">
-<img src="./image/ロゴ2.jpg" alt="城西大学のロゴ" width="420" height="220">
+function imgchange1() {document.getElementById('img1').innerHTML=img src="./image/ロゴ2.jpg" alt="城西大学のロゴ" width="420" height="220";}

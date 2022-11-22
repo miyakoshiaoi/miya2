3eb202e3c65690b674fd688ff2b0cd24095eb307
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2022-11-22 10:12:19 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -3,8 +3,7 @@ function hyoji(){
 var str="一文字ずつ表示します。";
 //テキストボックスの文字数
 var cnt=document.timer.moji.value.length;
-//文字が全部表示されているか確認}
-
+//文字が全部表示されているか確認 
   if(cnt<11){
   //現在より１文字多く切り出して表示
   document.timer.moji.value=str.substr(0,cnt+1);}
@@ -17,3 +16,4 @@ function startfnc(){
 
 function mes(){
   alert("3秒経ちました！");}
+

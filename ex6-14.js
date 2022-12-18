f93083d1fb99fb49b69ecf6e2b145f17980ebc99
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2022-12-18 21:16:03 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,8 @@
-function imgchange4(){document.getElementById('logo2').src='./image/josailogo.jpg';}
-function imgchange5(){document.getElementById('logo2').src='./image/josailogo2.jpg';setTimeout("imgchange4()",3000);}
+function imgchange4(){
+  document.getElementById('logo2').src=
+    './image/josailogo.jpg';}
+function imgchange5(){
+  document.getElementById('logo2').src=
+    './image/josailogo2.jpg';
+  setTimeout("imgchange4()",3000);
+}

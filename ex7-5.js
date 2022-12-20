a841df63c55e0f56788cfd8f9357c10ad80e35aa
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2022-12-20 10:15:38 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,3 +1,3 @@
 function nazo2(){
-  if(document.getEiementById('answer').value=='夢'){alert('あたり!');
+  if(document.getEiementById('answer').value=='夢'){alert('あたり!');}
                                                    else{alert(はずれ);}

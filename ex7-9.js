514743b4512164799c06398d4ac447aeb7d2386c
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2023-01-16 20:13:01 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,17 @@
+function nazo4(){
+if(document.getElementById('answer4').value=='夢'){
+ alert('当たり！');
+}
+else if(document.getElementById('answer4').value=='ゆめ'){
+ alert('当たり！');
+}
+else if(document.getElementById('answer4').value=='ユメ'){
+ alert('当たり！');
+}
+else if(document.getElementById('answer4').value=='yume'){
+ alert('当たり！');
+}
+else{
+ alert('はずれ!');
+ }
+}
